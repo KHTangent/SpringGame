5e6f22a2682925e8d37be0c9a905dfea4d8dd057
KHTangent <contact@khtangent.com>
2023-04-16 11:08:28 +0200

    Add explosion sound

--- source/objects/Bomb.hx
+++ source/objects/Bomb.hx
@@ -1,16 +1,22 @@
 package objects;
 
+import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.effects.particles.FlxEmitter.FlxTypedEmitter;
+import flixel.system.FlxSound;
 import flixel.util.FlxColor;
 
 class Bomb extends FlxSprite {
+	private var explosionSound:FlxSound;
+
 	public function new(x:Float, y:Float) {
 		super(x, y);
 		loadGraphic("assets/images/misc/bomb.png", false, 48, 48);
+		explosionSound = FlxG.sound.load("assets/sounds/bomb.wav");
 	}
 
 	public function explode() {
+		explosionSound.play();
 		var explosion = new FlxTypedEmitter(x + width / 2, y + height / 2, 120);
 		explosion.loadParticles("assets/images/misc/particle.png", 256);
 		explosion.alpha.set(0.3, 1);
