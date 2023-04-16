af0fc90698dbc85a2c2a12da0cbf6a6c820ccb4a
KHTangent <contact@khtangent.com>
2023-04-16 14:38:42 +0200

    Implement blip sound

--- source/objects/Springroll.hx
+++ source/objects/Springroll.hx
@@ -1,12 +1,17 @@
 package objects;
 
+import flixel.system.FlxSound;
+import flixel.FlxG;
 import flixel.FlxSprite;
 
 class Springroll extends FlxSprite {
 	public var value:Int;
 
+	private var collectSound:FlxSound;
+
 	public function new(x:Float, y:Float, value:Int) {
 		super(x, y);
+		collectSound = FlxG.sound.load("assets/sounds/blip.wav", 0.5);
 		this.value = value;
 		if (value < 40) {
 			loadGraphic("assets/images/misc/springRoll.png", true, 48, 48);
@@ -16,5 +21,10 @@ class Springroll extends FlxSprite {
 		}
 		animation.add("spin", [0, 1, 2, 3, 4], 10, true);
 		animation.play("spin");
+	}
+
+	override function kill() {
+		collectSound.play(true);
+		super.kill();
 	}
 }
